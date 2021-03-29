fd7e294a109f0eacfd7e9210b2fc41a6337f7cdc
Javier <javier@eskura21.com>
2021-03-29 18:46:38 +0200

    Creación de componente de formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListadoComponent } from './componentes/listado/listado.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListadoComponent
+    ListadoComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
