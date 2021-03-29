918f61d6440eea7409b012837d4ba57f5147fe77
Javier <javier@eskura21.com>
2021-03-29 20:02:27 +0200

    Rutados y router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+import { ListadoComponent } from './componentes/listado/listado.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/listado', pathMatch: 'full'},
+  { path: 'listado', component: ListadoComponent },
+  { path: 'formulario', component: FormularioComponent },
+  { path: 'formulario/:id', component: FormularioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
