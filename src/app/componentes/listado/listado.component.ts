bfbaa2fa0020bec4d8a19d541a79720c2f475c81
Javier <javier@eskura21.com>
2021-03-29 19:03:55 +0200

    El componente listado contiene el modelo

--- src/app/componentes/listado/listado.component.ts
+++ src/app/componentes/listado/listado.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Cliente } from 'src/app/modelos/cliente';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ListadoComponent {
+  clientes: Cliente[] = [
+    { id: 1, nombre: 'Javier', apellidos: 'Lete' },
+    { id: 2, nombre: 'Pepillo', apellidos: 'Pérez' },
+  ];
 }
