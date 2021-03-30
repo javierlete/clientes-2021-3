2049dd7a50b9e4b796af67dfd40e47c50ba00a67
Javier <javier@eskura21.com>
2021-03-30 20:41:12 +0200

    Corrección de los test de Jasmine (Karma)

--- src/app/componentes/formulario/formulario.component.spec.ts
+++ src/app/componentes/formulario/formulario.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './../../app-routing.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 
 import { FormularioComponent } from './formulario.component';
 
@@ -8,7 +11,8 @@ describe('FormularioComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormularioComponent ]
+      declarations: [ FormularioComponent ],
+      imports: [ AppRoutingModule, HttpClientModule ]
     })
     .compileComponents();
   });
--- src/app/componentes/listado/listado.component.spec.ts
+++ src/app/componentes/listado/listado.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListadoComponent } from './listado.component';
@@ -8,7 +9,8 @@ describe('ListadoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListadoComponent ]
+      declarations: [ ListadoComponent ],
+      imports: [ HttpClientModule ]
     })
     .compileComponents();
   });
